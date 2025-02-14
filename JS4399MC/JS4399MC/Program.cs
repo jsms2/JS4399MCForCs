17c547c898600cd9308326f53cb02cb09ed6dd71
jsms2 <jsgzem@163.com>
2025-02-15 00:24:12 +0800

    Add files via upload

---
+++ JS4399MC/JS4399MC/Program.cs
@@ -0,0 +1,41 @@
+﻿using JS4399MC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new JS4399HttpConfig
+        {
+        };
+
+        var js4399 = new JS4399(config);
+
+        Func<string, Task<string>> captchaFunc = async (base64) =>
+        {
+            string code = Ocr.GetOcrResult(base64);
+            return code;
+        };
+
+
+        var registerResult = await js4399.JS4399RegisterAsync(null, captchaFunc);
+        Console.WriteLine(registerResult.Success ?
+            $"注册成功: {registerResult.Username}/{registerResult.Password}" :
+            $"注册失败: {registerResult.Message}");
+        if (!registerResult.Success) return;
+        var loginParams = new Dictionary<string, object>{
+                { "username", registerResult.Username },
+                { "password", registerResult.Password }
+            };
+
+        var loginResult = await js4399.JS4399LoginAsync(loginParams, captchaFunc);
+        Console.WriteLine(loginResult.Success ?
+            $"登录成功:\n{loginResult.SauthJson}" :
+            $"登录失败: {loginResult.Message}");
+        Console.ReadLine();
+    }
+}
---
+++ JS4399MC/JS4399MC/ocr.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+
+internal class Ocr
+{
+    // 导入ocr.dll中的ocr4399函数
+    [DllImport("ocr.dll")]
+    public static extern string ocr4399(string imageBase64);
+
+    public static string GetOcrResult(string imageBase64)
+    {
+
+        // 将返回的 IntPtr 转换为字符串
+        return ocr4399(imageBase64);
+    }
+}
+
